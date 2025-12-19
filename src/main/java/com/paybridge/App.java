48cb281339fdfcfbc8d180a8d554ece82bd0afca
Hernán Martín <hernan.rodriguez.0824@gmail.com>
2025-12-19 10:09:39 -0300

    first commit

---
+++ src/main/java/com/paybridge/App.java
@@ -0,0 +1,11 @@
+package com.paybridge;
+
+/**
+ * Clase principal de PayBridge
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido a PayBridge!");
+    }
+}
+
